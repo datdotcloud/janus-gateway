1ade315d660e3ec142467fc6d17aaac0697b433f
Steven Acres <steven@mcprohosting.com>
2016-05-18 20:35:38 +0000

    disabled unused features on janus in build script

--- build_scripts/janus.bash
+++ build_scripts/janus.bash
@@ -1,10 +1,14 @@
 yum -y install libnice-janus libsrtp-janus libwebsockets-janus mysql mysql-devel mysql-lib glib2-devel
 export PKG_CONFIG_PATH=/opt/janus/lib/pkgconfig/
-git clone --recursive https://github.com/WatchBeam/janus-gateway.git
-cd janus-gateway
+#git clone --recursive https://github.com/WatchBeam/janus-gateway.git
+cd ..
+git submodule update --init
 #need to manually specify path to libwebsockets as configure script isnt using ldconfig for it (FIX ME)
 sh autogen.sh
-./configure --prefix=/opt/janus --disable-data-channels --disable-docs LDFLAGS="-L/opt/libwebsockets/lib -Wl,-rpath=/opt/libwebsockets/lib" CFLAGS="-I/opt/libwebsockets/include" --disable-rabbitmq
+PLUGIN_OPTS="--disable-plugin-audiobridge --disable-plugin-echotest --disable-plugin-recordplay --disable-plugin-sip \
+	--disable-plugin-streaming --disable-plugin-videocall --disable-plugin-videoroom --disable-plugin-voicemail"
+GENERAL_OPTS="--disable-data-channels --disable-docs --disable-rabbitmq --disable-unix-sockets"
+./configure --prefix=/opt/janus $PLUGIN_OPTS $GENERAL_OPTS LDFLAGS="-L/opt/libwebsockets/lib -Wl,-rpath=/opt/libwebsockets/lib" CFLAGS="-I/opt/libwebsockets/include"
 make
 if [[ -z "$1" ]]; then
 echo -e "\e[7mInstalling to /opt/janus\e[27m"
