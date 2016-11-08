03b5f0eecf8a7f4ae14827a4dcde19507e8dcff5
sslivins <sslivins@gmail.com>
2016-11-08 09:54:30 -0800

    updated to build impstar

--- build_scripts/make_janus_rpm.bash
+++ build_scripts/make_janus_rpm.bash
@@ -3,8 +3,8 @@ echo "Error: path to janus files not specified"
 exit -1
 fi
 cp janus.service $1/opt/janus/etc/
-fpm --description "Janus WebRTC Gateway with Beam FTL Plex plugin" \
-	-n janus-plex -v 1.2.8d -t rpm -s dir -C $1 -p NAME_VERSION_ARCH.rpm \
+fpm --description "Janus WebRTC Gateway with Beam FTL Impstar plugin" \
+	-n janus-impstar -v 1.2.8d -t rpm -s dir -C $1 -p NAME_VERSION_ARCH.rpm \
 	--after-install janus_post_install \
 	--after-remove janus_post_remove \
 	
