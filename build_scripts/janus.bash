cbb314bc386a8fab9fd2576c4831a1666c59c4a8
Stefan Slivinski <steven@mcprohosting.com>
2017-01-24 18:30:04 +0000

    removed gprof, added optimization level 3

--- build_scripts/janus.bash
+++ build_scripts/janus.bash
@@ -8,7 +8,7 @@ sh autogen.sh
 PLUGIN_OPTS="--disable-plugin-textroom --disable-plugin-audiobridge --disable-plugin-echotest --disable-plugin-recordplay --disable-plugin-sip \
 	--disable-plugin-streaming --disable-plugin-videocall --disable-plugin-videoroom --disable-plugin-voicemail"
 GENERAL_OPTS="--disable-data-channels --disable-docs --disable-rabbitmq --disable-unix-sockets --disable-mqtt --enable-libsrtp2"
-./configure --prefix=/opt/janus $PLUGIN_OPTS $GENERAL_OPTS CFLAGS='-pg'
+./configure --prefix=/opt/janus $PLUGIN_OPTS $GENERAL_OPTS CFLAGS='-O2'
 #./configure --prefix=/opt/janus $PLUGIN_OPTS $GENERAL_OPTS LDFLAGS="-L/lib64 -Wl,-rpath=/lib64" CFLAGS="-I/usr/include"
 make clean
 make
