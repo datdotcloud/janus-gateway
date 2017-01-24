e920d835d8d18120fbfde325992b3295a79fa7fb
Stefan Slivinski <steven@mcprohosting.com>
2017-01-24 18:13:02 +0000

    added compiler flag to enable gprof

--- build_scripts/janus.bash
+++ build_scripts/janus.bash
@@ -8,8 +8,9 @@ sh autogen.sh
 PLUGIN_OPTS="--disable-plugin-textroom --disable-plugin-audiobridge --disable-plugin-echotest --disable-plugin-recordplay --disable-plugin-sip \
 	--disable-plugin-streaming --disable-plugin-videocall --disable-plugin-videoroom --disable-plugin-voicemail"
 GENERAL_OPTS="--disable-data-channels --disable-docs --disable-rabbitmq --disable-unix-sockets --disable-mqtt --enable-libsrtp2"
-./configure --prefix=/opt/janus $PLUGIN_OPTS $GENERAL_OPTS
+./configure --prefix=/opt/janus $PLUGIN_OPTS $GENERAL_OPTS CFLAGS='-pg'
 #./configure --prefix=/opt/janus $PLUGIN_OPTS $GENERAL_OPTS LDFLAGS="-L/lib64 -Wl,-rpath=/lib64" CFLAGS="-I/usr/include"
+make clean
 make
 if [[ -z "$1" ]]; then
 echo -e "\e[7mInstalling to /opt/janus-impstar\e[27m"
