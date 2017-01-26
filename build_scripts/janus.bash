25a458d354709f3b0fd7f848228b37a4b79be09f
Stefan Slivinski <steven@mcprohosting.com>
2017-01-26 04:58:45 +0000

    enabled boring seel

--- build_scripts/janus.bash
+++ build_scripts/janus.bash
@@ -7,7 +7,7 @@ cd ..
 sh autogen.sh
 PLUGIN_OPTS="--disable-plugin-textroom --disable-plugin-audiobridge --disable-plugin-echotest --disable-plugin-recordplay --disable-plugin-sip \
 	--disable-plugin-streaming --disable-plugin-videocall --disable-plugin-videoroom --disable-plugin-voicemail"
-GENERAL_OPTS="--disable-data-channels --disable-docs --disable-rabbitmq --disable-unix-sockets --disable-mqtt --enable-libsrtp2"
+GENERAL_OPTS="--disable-data-channels --disable-docs --disable-rabbitmq --disable-unix-sockets --disable-mqtt --enable-libsrtp2 --enable-boringssl"
 ./configure --prefix=/opt/janus $PLUGIN_OPTS $GENERAL_OPTS
 #./configure --prefix=/opt/janus $PLUGIN_OPTS $GENERAL_OPTS LDFLAGS="-L/lib64 -Wl,-rpath=/lib64" CFLAGS="-I/usr/include"
 make clean
