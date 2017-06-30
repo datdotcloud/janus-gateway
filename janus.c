2bea5fa0afa5f7fce8a4452792bf41ca5a59660e
Stefan Slivinski <sslivins@gmail.com>
2017-06-30 11:55:28 -0700

    treat specific fields as ints even if they are reals

--- janus.c
+++ janus.c
@@ -130,6 +130,19 @@ gint janus_is_stopping(void)
   return g_atomic_int_get(&stop);
 }
 
+guint64 _json_get_int64(json_t *s)
+{
+  if (s && json_is_number(s)) {
+    if(json_is_integer(s)){
+      session_id = json_integer_value(s);
+    }
+    else {
+      session_id = (guint64)json_real_value(s);
+    }
+  }
+}
+
+
 /* Public instance name */
 static gchar *server_name = NULL;
 
@@ -574,18 +587,12 @@ int janus_process_incoming_request(janus_request *request)
   /* Ok, let's start with the ids */
   guint64 session_id = 0, handle_id = 0;
   json_t *s = json_object_get(root, "session_id");
-  if (s && json_is_number(s)) {
-    if(json_is_integer(s)){
-      session_id = json_integer_value(s);
-    }
-    else {
-      session_id = (guint64)json_real_value(s);
-    }
-  }
+
+  session_id = _json_get_int64(s);
 
   json_t *h = json_object_get(root, "handle_id");
-  if (h && json_is_integer(h))
-    handle_id = json_integer_value(h);
+
+  handle_id = _json_get_int64(h);
 
   /* Get transaction and message request */
   JANUS_VALIDATE_JSON_OBJECT(root, incoming_request_parameters,
@@ -663,7 +670,8 @@ int janus_process_incoming_request(janus_request *request)
     if (id != NULL)
     {
       /* The application provided the session ID to use */
-      session_id = json_integer_value(id);
+      //session_id = json_integer_value(id);
+      session_id = _json_get_int64(id);
       if (session_id > 0 && janus_session_find(session_id) != NULL)
       {
         /* Session ID already taken */
